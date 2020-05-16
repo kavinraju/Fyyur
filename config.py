aa9d05d3955aafdca056d32f163ba4ce04b1b422
kavinraju <rajukavin25@gmail.com>
2020-05-17 00:43:53 +0530

    added env_file

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
+from env_file import heroku_database_uri_env
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 # Heroku DB URI
-heroku_database_uri ='postgres://zsfglqnhlmteob:1af5477353d7095d1fa599ed095609bbb2eeb762ac8424d36e38cedc10d92b4f@ec2-54-175-117-212.compute-1.amazonaws.com:5432/d1iafsk3io5bgl'
+heroku_database_uri = heroku_database_uri_env
 # Local DB Details
 database_name = "fyyur_heruko"
 username = "postgres"
